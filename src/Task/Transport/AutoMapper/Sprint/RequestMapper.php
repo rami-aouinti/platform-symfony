bd2f22ac328e1a68ed3fc1af91a7d3413140f1a8
Rami <rami.aouinti@gmail.com>
2026-02-27 00:28:18 +0100

    feat(task): allow assigning multiple task requests to sprint

--- src/Task/Transport/AutoMapper/Sprint/RequestMapper.php
+++ src/Task/Transport/AutoMapper/Sprint/RequestMapper.php
@@ -5,14 +5,25 @@ declare(strict_types=1);
 namespace App\Task\Transport\AutoMapper\Sprint;
 
 use App\General\Transport\AutoMapper\RestRequestMapper;
+use App\Task\Application\Resource\Interfaces\TaskRequestResourceInterface;
+use App\Task\Domain\Entity\TaskRequest;
 use DateTimeImmutable;
+use Throwable;
+
+use function array_filter;
+use function array_map;
 
 class RequestMapper extends RestRequestMapper
 {
     protected static array $properties = [
         'startDate',
         'endDate',
+        'taskRequests',
     ];
+
+    public function __construct(private readonly TaskRequestResourceInterface $taskRequestResource)
+    {
+    }
 
     protected function transformStartDate(?string $startDate): ?DateTimeImmutable
     {
@@ -22,5 +33,23 @@ class RequestMapper extends RestRequestMapper
     protected function transformEndDate(?string $endDate): ?DateTimeImmutable
     {
         return $endDate !== null && $endDate !== '' ? new DateTimeImmutable($endDate) : null;
+    }
+
+    /**
+     * @param array<int, string>|string|null $taskRequests
+     *
+     * @return array<int, TaskRequest>
+     */
+    protected function transformTaskRequests(array|string|null $taskRequests): array
+    {
+        $taskRequestIds = (array) $taskRequests;
+
+        return array_values(array_filter(array_map(function (string $taskRequestId): ?TaskRequest {
+            try {
+                return $this->taskRequestResource->getReference($taskRequestId);
+            } catch (Throwable) {
+                return null;
+            }
+        }, $taskRequestIds)));
     }
 }
