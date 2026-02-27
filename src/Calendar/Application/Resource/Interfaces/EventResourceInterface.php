f3e40fdf4d83d94b5db92fcaf672a82c02185e77
Rami <rami.aouinti@gmail.com>
2026-02-27 03:30:14 +0100

    fix calendar event resource interface import path

--- src/Calendar/Application/Resource/Interfaces/EventResourceInterface.php
+++ src/Calendar/Application/Resource/Interfaces/EventResourceInterface.php
@@ -4,7 +4,7 @@ declare(strict_types=1);
 
 namespace App\Calendar\Application\Resource\Interfaces;
 
-use App\General\Application\Resource\Interfaces\RestResourceInterface;
+use App\General\Application\Rest\Interfaces\RestResourceInterface;
 
 interface EventResourceInterface extends RestResourceInterface
 {
