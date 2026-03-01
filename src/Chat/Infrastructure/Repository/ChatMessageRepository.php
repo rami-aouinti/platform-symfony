2c9d8376d364337039c400771a602baef9ffc07c
Rami <rami.aouinti@gmail.com>
2026-03-01 03:08:05 +0100

    Fix Chat repositories return types for BaseRepository compatibility

--- src/Chat/Infrastructure/Repository/ChatMessageRepository.php
+++ src/Chat/Infrastructure/Repository/ChatMessageRepository.php
@@ -59,13 +59,13 @@ class ChatMessageRepository extends BaseRepository implements ChatMessageReposit
             ->getResult();
     }
 
-    public function save(Entity $message, ?bool $flush = null): ChatMessageRepositoryInterface
+    public function save(Entity $message, ?bool $flush = null, ?string $entityManagerName = null): self
     {
-        return parent::save($message, $flush);
+        return parent::save($message, $flush, $entityManagerName);
     }
 
-    public function remove(Entity $message, ?bool $flush = null): ChatMessageRepositoryInterface
+    public function remove(Entity $message, ?bool $flush = null, ?string $entityManagerName = null): self
     {
-        return parent::remove($message, $flush);
+        return parent::remove($message, $flush, $entityManagerName);
     }
 }
