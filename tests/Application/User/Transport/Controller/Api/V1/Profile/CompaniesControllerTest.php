848303357dbd7115a0c490e247b3e48a678f89f4
Rami <rami.aouinti@gmail.com>
2026-03-01 21:21:25 +0100

    Add profile and my-company membership API tests

---
+++ tests/Application/User/Transport/Controller/Api/V1/Profile/CompaniesControllerTest.php
@@ -0,0 +1,103 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Tests\Application\User\Transport\Controller\Api\V1\Profile;
+
+use App\General\Domain\Utils\JSON;
+use App\Tests\TestCase\WebTestCase;
+use Symfony\Component\HttpFoundation\Response;
+use Throwable;
+
+class CompaniesControllerTest extends WebTestCase
+{
+    private const string COMPANIES_URL = self::API_URL_PREFIX . '/v1/me/profile/companies';
+    private const string PROJECTS_URL = self::API_URL_PREFIX . '/v1/me/profile/projects';
+
+    /**
+     * @throws Throwable
+     */
+    public function testCompaniesEndpointReturnsOnlyCurrentUserCompanies(): void
+    {
+        $johnClient = $this->getTestClient('john-user', 'password-user');
+        $johnClient->request('GET', self::COMPANIES_URL);
+
+        self::assertSame(Response::HTTP_OK, $johnClient->getResponse()->getStatusCode());
+
+        $johnCompanies = JSON::decode((string)$johnClient->getResponse()->getContent(), true);
+        self::assertIsArray($johnCompanies);
+        self::assertCount(2, $johnCompanies);
+
+        $johnCompanyIds = array_column($johnCompanies, 'id');
+        self::assertEqualsCanonicalizing(
+            [
+                '30000000-0000-1000-8000-000000000001',
+                '30000000-0000-1000-8000-000000000008',
+            ],
+            $johnCompanyIds,
+        );
+
+        foreach ($johnCompanies as $company) {
+            self::assertArrayHasKey('id', $company);
+            self::assertArrayHasKey('legalName', $company);
+            self::assertArrayHasKey('status', $company);
+            self::assertArrayHasKey('photoUrl', $company);
+        }
+
+        $carolClient = $this->getTestClient('carol-user', 'password-user');
+        $carolClient->request('GET', self::COMPANIES_URL);
+
+        self::assertSame(Response::HTTP_OK, $carolClient->getResponse()->getStatusCode());
+
+        $carolCompanies = JSON::decode((string)$carolClient->getResponse()->getContent(), true);
+        self::assertCount(1, $carolCompanies);
+        self::assertSame('30000000-0000-1000-8000-000000000005', (string)$carolCompanies[0]['id']);
+    }
+
+    /**
+     * @throws Throwable
+     */
+    public function testProjectsEndpointReturnsOnlyAuthorizedProjects(): void
+    {
+        $johnClient = $this->getTestClient('john-user', 'password-user');
+        $johnClient->request('GET', self::PROJECTS_URL);
+
+        self::assertSame(Response::HTTP_OK, $johnClient->getResponse()->getStatusCode());
+
+        $johnProjects = JSON::decode((string)$johnClient->getResponse()->getContent(), true);
+        self::assertIsArray($johnProjects);
+        self::assertCount(3, $johnProjects);
+        self::assertSame(
+            [
+                '70000000-0000-1000-8000-000000000006',
+                '70000000-0000-1000-8000-000000000001',
+                '70000000-0000-1000-8000-000000000007',
+            ],
+            array_column($johnProjects, 'id'),
+        );
+
+        foreach ($johnProjects as $project) {
+            self::assertArrayHasKey('id', $project);
+            self::assertArrayHasKey('name', $project);
+            self::assertArrayHasKey('description', $project);
+            self::assertArrayHasKey('status', $project);
+            self::assertArrayHasKey('photoUrl', $project);
+        }
+
+        $carolClient = $this->getTestClient('carol-user', 'password-user');
+        $carolClient->request('GET', self::PROJECTS_URL);
+
+        self::assertSame(Response::HTTP_OK, $carolClient->getResponse()->getStatusCode());
+
+        $carolProjects = JSON::decode((string)$carolClient->getResponse()->getContent(), true);
+        self::assertIsArray($carolProjects);
+        self::assertCount(2, $carolProjects);
+        self::assertSame(
+            [
+                '70000000-0000-1000-8000-000000000005',
+                '70000000-0000-1000-8000-000000000004',
+            ],
+            array_column($carolProjects, 'id'),
+        );
+    }
+}
